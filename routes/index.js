b89283322165e7e12d54f09f3d03ed92e4da60b5
Jean Assis <jean.assis@gft.com>
2023-01-30 13:33:20 -0300

    Multiple endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,66 @@
-var express = require('express');
-var httpsAgent = require('https-agent');
-var fetchDataSSL = require('../services/requests.service');
-var fetchData = require('../services/requests.service');
-var fs = require('fs');
-var path = require("path");
-var convert = require('json-to-plain-text');
+var express = require("express");
+var fetchData = require("../services/requests.service");
 var router = express.Router();
 
-
-var INSTITUITIONS_ENDPOINT = 'https://data.directory.openbankingbrasil.org.br/participants';
-
-var endpoint = '/products-services/v1/personal-unarranged-account-overdraft';
-
+var INSTITUITIONS_ENDPOINT =
+  "https://data.directory.openbankingbrasil.org.br/participants";
+var bodyData = "";
 
 async function retriveData(param) {
+  if (!param) return Promise.resolve([]);
   const datalist = [];
-  const response = await fetchData(INSTITUITIONS_ENDPOINT)
+  const response = await fetchData(INSTITUITIONS_ENDPOINT);
 
-  /*   const agt = httpsAgent({
-      rejectUnauthorized: false, // (NOTE: this will disable client verification)
-      cert: fs.readFileSync(path.resolve(__dirname, '../public/certs/usercert.pem')),
-      key: fs.readFileSync(path.resolve(__dirname, '../public/certs/key.pem')),
-      passphrase: "YYY"
-  }) */
+  const endpoints = param.split(",");
+  console.log("Endpoints ---> ", endpoints);
 
   return new Promise(async (resolve, reject) => {
-    for (const iterator of response.data) {
-
-      if (!!iterator.AuthorisationServers[0]?.ApiResources[0]?.ApiDiscoveryEndpoints[0]?.ApiEndpoint) {
-        const url = iterator.AuthorisationServers[0].ApiResources[0].ApiDiscoveryEndpoints[0].ApiEndpoint;
-        const { hostname, protocol } = new URL(url);
-        const host = `${protocol}//${hostname}/open-banking${param ? param : endpoint}`
-        try {
-
-          const bankRes = await fetchData(host)
-
-          datalist.push({ OrganisationName: iterator.OrganisationName, endpoint: host, responseData: bankRes.data })
-        } catch (error) {
-          console.error(error)
-          datalist.push({ OrganisationName: iterator.OrganisationName, endpoint: host, responseData: error.data || error.code })
+    endpoints.forEach(async (ep, index) => {
+      const endpointData = [];
+      ep = ep.replace(/(\r\n|\n|\r)/gm, "");
+      for (const iterator of response.data) {
+        if (
+          !!iterator.AuthorisationServers[0]?.ApiResources[0]
+            ?.ApiDiscoveryEndpoints[0]?.ApiEndpoint
+        ) {
+          const url =
+            iterator.AuthorisationServers[0].ApiResources[0]
+              .ApiDiscoveryEndpoints[0].ApiEndpoint;
+          const { hostname, protocol } = new URL(url);
+          const target = `${protocol}//${hostname}/open-banking${ep}`;
+          try {
+            const bankRes = await fetchData(target);
+            endpointData.push({
+              OrganisationName: iterator.OrganisationName,
+              endpoint: target,
+              responseData: bankRes.data,
+            });
+          } catch (error) {
+            console.error(error);
+            endpointData.push({
+              OrganisationName: iterator.OrganisationName,
+              endpoint: target,
+              responseData: error.data || error.code,
+            });
+          }
         }
-
       }
-    }
+      datalist.push({ endpoint: ep, data: endpointData });
+    });
     resolve(datalist);
   });
 }
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  const result = await retriveData();
-  res.render('index', { taskList: result });
+router.get("/", async function (req, res, next) {
+  const result = await retriveData(bodyData);
+  res.render("index", { taskList: result });
 });
 
-router.post('/refresh', async function (req, res, next) {
-  const result = await retriveData(req.body.target);
-  res.render('index', { taskList: result });
+router.post("/", async function (req, res, next) {
+  bodyData = req.body.target;
+  const result = await retriveData(bodyData);
+  res.send({ taskList: result });
 });
 
 module.exports = router;
